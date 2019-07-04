c76aff2db8e8b03221b545d9dcfb1ea057520072
Nishad <nishadpathoor@gmail.com>
2019-07-04 10:10:01 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn npws.wsgi --log-file -
---
+++ src/Customer/CustomersService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+const API_URL = 'http://127.0.0.1:8000';
+
+export default class CustomerService{
+	constructor(){}
+	
+	getCustomers(){
+		const url = `${API_URL}/api/customers/`;
+		return axios.get(url).then(response => response.data);
+	}
+	getCustomerByURL(link){
+		const url = `${API_URL}${link}`;
+		return axios.get(url).then(response => response.data);
+	}
+	getCustomer(pk){
+		const url = `${API_URL}/api/customers/${pk}`;
+		return axios.get(url).then(response => response.data);
+	}
+	deleteCustomer(customer){
+		const url = `${API_URL}/api/customers/${customer.pk}`;
+		return axios.delete(url);
+	}
+	createCustomer(customer){
+		const url = `${API_URL}/api/customers/`;
+		return axios.post(url,customer);
+	}
+	updateCustomer(customer){
+		const url = `${API_URL}/api/customers/${customer.pk}`;
+		return axios.put(url,customer);
+	}
+}
