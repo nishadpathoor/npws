6701679d7cfc327ef37d94b0e3ca806faf54636c
Nishad <nishadpathoor@gmail.com>
2019-07-04 11:27:42 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 const BaseLayout = () => (
   <div className="container-fluid">
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Django React Demo</a>
+  <a className="navbar-brand" href="#">npws</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
--- src/Customer/CustomersService.js
+++ src/Customer/CustomersService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://npws.herokuapp.com';
 
 export default class CustomerService{
 	constructor(){}
